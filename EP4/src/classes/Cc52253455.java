da9485dcfe34515482e2ce130f697b9c9bae5754
Alan Cardoso <alan_accardoso@yahoo.com.br>
2016-03-09 21:20:59 -0300

    Added files via upload

---
+++ EP4/src/classes/Cc52253455.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package classes;
+
+/**
+ * @author alan_
+ *
+ */
+public class Cc52253455 {
+
+	/**
+	 * 
+	 */
+	public Cc52253455() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
