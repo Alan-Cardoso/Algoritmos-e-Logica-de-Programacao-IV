9989100cb8825af849cbc9bc8f362f9ce50df135
Alan Cardoso <alan_accardoso@yahoo.com.br>
2016-03-15 12:17:16 -0300

    Added files via upload
    Implementando código EP4 Parte 01

---
+++ EP4/src/classes/ModeloPadrao.java
@@ -0,0 +1,118 @@
+package classes;
+
+/**
+ * @author alan_
+ *
+ */
+public class ModeloPadrao {// Fazer método main
+	
+	public void descongelar() {
+
+	}
+
+	public void cozinhar() {
+
+	}
+
+	public void timer() {
+
+	}
+
+	public void fazerPipoca() {
+
+	}
+
+	public void fazerCha() {
+
+	}
+
+	public void grelhar() {
+
+	}
+}
+
+class ModeloEconomico extends ModeloPadrao {
+
+	@Override
+	public void descongelar() {
+		// TODO Auto-generated method stub
+		super.descongelar();
+	}
+
+	@Override
+	public void cozinhar() {
+		// TODO Auto-generated method stub
+		super.cozinhar();
+	}
+
+	@Override
+	public void timer() {
+		// TODO Auto-generated method stub
+		super.timer();
+	}
+
+	@Override
+	public void fazerPipoca() {
+		// TODO Auto-generated method stub
+		super.fazerPipoca();
+	}
+
+	@Override
+	public void fazerCha() {
+		// TODO Auto-generated method stub
+		super.fazerCha();
+	}
+
+	@Override
+	public void grelhar() {
+		// TODO Auto-generated method stub
+		super.grelhar();
+	}
+	
+	public void cozimentoBaixo() {
+		
+	}
+}
+
+class ModeloPotente extends ModeloPadrao {
+
+	@Override
+	public void descongelar() {
+		// TODO Auto-generated method stub
+		super.descongelar();
+	}
+
+	@Override
+	public void cozinhar() {
+		// TODO Auto-generated method stub
+		super.cozinhar();
+	}
+
+	@Override
+	public void timer() {
+		// TODO Auto-generated method stub
+		super.timer();
+	}
+
+	@Override
+	public void fazerPipoca() {
+		// TODO Auto-generated method stub
+		super.fazerPipoca();
+	}
+
+	@Override
+	public void fazerCha() {
+		// TODO Auto-generated method stub
+		super.fazerCha();
+	}
+
+	@Override
+	public void grelhar() {
+		// TODO Auto-generated method stub
+		super.grelhar();
+	}
+	
+	public void cozimentoAlto() {
+		
+	}
+}
