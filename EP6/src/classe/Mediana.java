8853802e889e751a3aeb50157a14272d1bb6488e
Alan Cardoso <alan_accardoso@yahoo.com.br>
2016-03-30 01:58:25 -0300

    Added files via upload
    Alterações EP6.

--- EP6/src/classe/Mediana.java
+++ EP6/src/classe/Mediana.java
@@ -23,8 +23,8 @@ public class Mediana {
 		lista.add(0, 50);
 		lista.add(1, 25);
 		lista.add(2, 10);
-		lista.add(3, 70);
-		lista.add(4, 35);
+		lista.add(3, 35);
+		lista.add(4, 70);
 		lista.add(5, 55);
 		lista.add(6, 45);
 		lista.add(7, 20);
@@ -33,28 +33,34 @@ public class Mediana {
 	}
 
 	private static void calcularMediana(ArrayList<Integer> lista) {
-		int maiorNum = 0, menorNum = numero.get(0), i = 0;
-		boolean menor = false, maior = false;
+		Integer maiorNum = 0;
+		Integer menorNum = numero.get(0);
+		Integer i = 0, indiceMenor = null;
+		Integer indiceMaior = null;
+
 
 		if (numero.size() == 1) {
-			System.out.println("A valor da mediana da lista informada e de: " + numero.get(0));
+			System.out.println("A valor da mediana da lista informada e de: "  + numero.get(0));
 			System.exit(0);
 		}
 		if (numero.size() == 2) {
-			System.out.println("A valor da mediana da lista informada e de: " +(numero.get(0) + numero.get(1)) / 2);
+			// System.out.println("A valor da mediana da lista informada e de: "
+			// +(numero.get(0) + numero.get(1)) / 2);
 			System.exit(0);
 		}
-		while(i < numero.size()){
-			if (numero.get(i) <= menorNum) {
+		while (i < numero.size()) {
+			if (numero.get(i) < menorNum) {
 				menorNum = numero.get(i);
+				indiceMenor = i;
 			}
-			if (numero.get(i) >= maiorNum) {
+			if (numero.get(i) > maiorNum) {
 				maiorNum = numero.get(i);
+				indiceMaior = i;
 			}
 			i++;
 		}
-		numero.remove(menor);
-		numero.remove(maior);
-			calcularMediana(numero);
+		numero.remove(indiceMenor);
+		numero.remove(indiceMaior);
+		calcularMediana(numero);
 	}
 }
