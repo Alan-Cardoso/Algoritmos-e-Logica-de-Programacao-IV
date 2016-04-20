7c641498bb9b7a3b8c24e18e9c56d3249e18749e
Alan Cardoso <alan_accardoso@yahoo.com.br>
2016-04-20 15:25:43 -0300

    Implementação EP8 - parte 03

--- EP8/src/classe/No.java
+++ EP8/src/classe/No.java
@@ -11,101 +11,136 @@ public class No {
 	private static No esquerdo;
 	private static No direito;
 
-	@SuppressWarnings("unused")
 	public No(Integer valor) {
-		No.setValor(valor);
-		No esquerdo;
-		No direito;
+		setValor(valor);
 	}
-	
+
+	public static void main(String[] args) {
+		addNo(raiz, 50);
+		addNo(raiz, 25);
+	}
+
 	/**
 	 * Metodo para adicionar valores ordenados em uma arvore
 	 * 
 	 * @param no
 	 * @param valor
 	 */
-	@SuppressWarnings("unused")
 	private static void addNo(No no, Integer valor) {
-		/* Verifica se o valor da raiz da arvore é null.
-		 * Se a raiz for null será adicionado um novo No,
-		 * passando o novo valor a ser inserido
+		/*
+		 * Verifica se o valor da raiz da arvore é null. Se a raiz for null será
+		 * adicionado um novo No, passando o novo valor a ser inserido
 		 */
-		if(no == null){
+		if (no == null) {
 			no = new No(valor);
-		}else{
-			 /* Se a raiz não for null.
-			 * Vai ser verificado se o novo valor a ser inserido,
-			 * é maior que o valor da raiz.
+		} else {
+			/*
+			 * Aqui vai ser verificado se o novo valor a ser inserido, é menor
+			 * que o valor da raiz.
 			 */
-			if (valor > No.getValor()) {
-				 /* Se o novo valor a ser inserido for maior que a raiz.
-				 * Vai verificar se o lado direito da raiz é diferente null.
+			if (valor < No.getValor()) {
+				/*
+				 * Se o novo valor a ser inserido for menor que a raiz. Vai
+				 * verificar se o lado direito da raiz é diferente null.
+				 */
+				if (No.getEsquerdo() != null) {
+					/*
+					 * Se o lado direito da raiz é diferente null. Vai ser
+					 * inserido o novo valor a esse lado
+					 */
+					addNo(No.getEsquerdo(), valor);
+				} else {
+					/*
+					 * Se o lado direito da raiz é null. Vai ser inserido o novo
+					 * No com esse novo valor a esse lado
+					 */
+					No.setEsquerdo(new No(valor));
+				}
+				/*
+				 * Se a raiz não for null. Vai ser verificado se o novo valor a
+				 * ser inserido, é maior que o valor da raiz.
+				 */
+			} else if (valor > No.getValor()) {
+				/*
+				 * Se o novo valor a ser inserido for maior que a raiz. Vai
+				 * verificar se o lado direito da raiz é diferente null.
 				 */
 				if (No.getDireito() != null) {
-					/*Se o lado direito da raiz é diferente null.
-					 * Vai ser inserido o novo valor a esse lado
+					/*
+					 * Se o lado direito da raiz é diferente null. Vai ser
+					 * inserido o novo valor a esse lado
 					 */
 					addNo(No.getDireito(), valor);
 				} else {
-					/*Se o lado direito da raiz é null.
-					 * Vai ser inserido o novo No com esse novo valor a esse lado
+					/*
+					 * Se o lado direito da raiz é null. Vai ser inserido o novo
+					 * No com esse novo valor a esse lado
 					 */
 					No.setDireito(new No(valor));
 				}
 			}
-			/* Aqui vai ser verificado se o novo valor a ser inserido,
-			 * é menor que o valor da raiz.
-			 */
-			if (valor < No.getValor()) {
-				 /* Se o novo valor a ser inserido for menor que a raiz.
-				 * Vai verificar se o lado direito da raiz é diferente null.
-				 */
-				if (No.getEsquerdo()!= null) {
-					/*Se o lado direito da raiz é diferente null.
-					 * Vai ser inserido o novo valor a esse lado
-					 */
-					addNo(No.getEsquerdo(), valor);
+		}
+	}
+
+	/**
+	 * Metodopara imprimir arvore em ordem
+	 * 
+	 * @param no
+	 */
+	public static void emOrdem(No no) {
+		if (no != null) {
+			emOrdem(No.getEsquerdo());
+			System.out.print(No.getValor() + " ");
+			emOrdem(No.getDireito());
+		}
+	}
+
+	/**
+	 * Metodo que busca um valor na arvore
+	 * 
+	 * @param no
+	 * @param valor
+	 * @return
+	 */
+	public Object busca(No no, Integer valor) {// NÂO TERMINADO
+		if (No.getValor() == null) {
+			return "O valor " + valor + " não esta presente na arvore";
+		} else {
+			if (valor == No.getValor()) {
+				return "O valor " + valor + " esta presente na arvore";
+			} else {
+
+				if (valor > No.getValor()) {
+					if (No.getDireito() == null) {
+						return null;
+					}
+					return No.getDireito().busca(no, valor);
 				} else {
-					/*Se o lado direito da raiz é null.
-					 * Vai ser inserido o novo No com esse novo valor a esse lado
-					 */
-					No.setEsquerdo(new No(valor));
+					if (No.getEsquerdo() == null) {
+						return null;
+					}
+					return No.getEsquerdo().busca(no, valor);
 				}
 			}
 		}
 	}
 	
-	public static void emOrdem(No no) {
-		if(no != null){
-			emOrdem(No.getEsquerdo());
-			System.out.print(No.getValor() + " ");
-			emOrdem(No.getDireito());
-			}
-		}
-
-
-	public Object busca(No no, Integer valor) {
-        if (No.getValor() == null) {
-        	return "O valor "+valor+" não esta presente na arvore";
-        } else {
-            if (valor == No.getValor()) {
-                return "O valor "+valor+" esta presente na arvore";
-            } else {
-                if (valor > No.getValor()) {
-                    if (No.getDireito() == null) {
-                        return null;
-                    }
-                    return No.getDireito().busca(no, valor);
-                } else {
-                    if (No.getEsquerdo() == null) {
-                        return null;
-                    }
-                    return No.getEsquerdo().busca(no,valor);
-                }
-            }
-        }
+	/**
+	 * Metodo que retoena o valor minimo da arvore
+	 * 
+	 * @param no
+	 * @return
+	 */
+	@SuppressWarnings("unused")
+	private No minimo(No no) {// NÂO TERMINADO
+        if (no != null) {  
+            while (No.getEsquerdo() != null) {  
+                no = No.getEsquerdo();  
+            }  
+        }  
+        return no;  
     }
-	
+
 	public static No getRaiz() {
 		return raiz;
 	}
