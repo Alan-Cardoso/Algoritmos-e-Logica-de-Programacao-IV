ba3e28c0ca0f40e68e1a67b80299e3f2bc3b71cb
Alan Cardoso <alan_accardoso@yahoo.com.br>
2016-05-02 15:04:39 -0300

    Upload final EP9

--- EP9/src/classe/No.java
+++ EP9/src/classe/No.java
@@ -5,7 +5,7 @@ package classe;
 
 /**
  * @author AlanCardoso
- *
+ * EP9
  */
 public class No {
 
@@ -16,7 +16,15 @@ public class No {
 
 	public No(Integer valor) {
 		setValor(valor);
-
+	}
+	
+	/**
+	 * Método cria uma raiz com valor null.
+	 * 
+	 */
+	@SuppressWarnings("static-access")
+	public static void criaArvore(No no) {
+		no.valor = null;
 	}
 
 	/**
@@ -26,27 +34,48 @@ public class No {
 	 * @param valor
 	 */
 	@SuppressWarnings({ "static-access", "unused" })
-	private static void addNo(No no, Integer valor) {
+	private static void addArvore(No no, Integer valor) {
 
-		if (no == null) {
+		if (no.valor == null) {
 			no = new No(valor);
 		} else {
 			if (valor < no.getValor()) {
-				if (no.getEsquerdo() != null) {
-					addNo(no.getEsquerdo(), valor);
+				no = no.getEsquerdo();
+				if (no.getValor() != null) {
+					addArvore(no.getEsquerdo(), valor);
 				} else {
 					no.setEsquerdo(new No(valor));
 				}
-			} else if (valor > no.getValor()) {
-				if (no.getDireito() != null) {
-					addNo(no.getDireito(), valor);
+			} 
+			if (valor > no.getValor()) {
+				no = no.getDireito();
+				if (no.getValor() != null) {
+					addArvore(no.getDireito(), valor);
 				} else {
 					no.setDireito(new No(valor));
 				}
 			}
 		}
 	}
+	/**
+	 * Método para percorrer a Árvore em ordem.
+	 * 
+	 * @return 
+	 */
+	@SuppressWarnings("static-access")
+	public void percorreArvore(No no) {
+		 if( no != null ) { 
+             System.out.println(no.valor);
+		 }
+		 percorreArvore( no.getEsquerdo());    
+         percorreArvore ( no.getDireito() ); 
+	}
 
+	/**
+	 * Métodos Gether and Seters nos atributos de Nó.
+	 * 
+	 * @return
+	 */
 	public static No getRaiz() {
 		return raiz;
 	}
